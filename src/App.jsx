69fcaaf668f247d6a5bbfb087a03fe67280c9ca6
Sinisa <sinisha.stojanovic@gmail.com>
2024-07-12 22:49:56 +0200

    General background created

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,7 @@
 import "./App.css";
 
 function App() {
-  return (
-    <>
-      <h1>Hello</h1>
-      <p className={"ime"}>Sinisa Stojanovic</p>
-    </>
-  );
+  return <></>;
 }
 
 export default App;
