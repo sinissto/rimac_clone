0945368f96681ad9b8406089a34a7640680cd4df
Sinisa <sinisha.stojanovic@gmail.com>
2024-07-14 00:37:19 +0200

    Landing Page Data collected

---
+++ src/data/mainData.js
@@ -0,0 +1,34 @@
+export const mainData = [
+  {
+    id: 1,
+    title: "Nevera",
+    description:
+      "Capable of exceptional speeds, swift and powerful beyond comprehension, Nevera is a force like no other. Designed, engineered and handcrafted in Croatia, defined by function and forged from a love for automotive.",
+    note: "WLTP: electricity consumption, combined 30.0 KWh/100km; CO2 emissions: combined in g/km: 0",
+    btnContent: "Discover",
+  },
+  {
+    id: 2,
+    title: "Technology",
+    description:
+      "From bespoke component design and engineering to full series production. We are the leader in high-performance EV technology.",
+    note: "",
+    btnContent: "Explore",
+  },
+  {
+    id: 3,
+    title: "Development",
+    description:
+      "We are taking you behind the scenes of globally homologated electric hypercar design, engineering and testing.",
+    note: "",
+    btnContent: "Discover",
+  },
+  {
+    id: 4,
+    title: "About Us",
+    description:
+      "We challenge convention and push technology to the edge of possibility. Welcome to Rimac. This is our story.",
+    note: "",
+    btnContent: "Explore",
+  },
+];
